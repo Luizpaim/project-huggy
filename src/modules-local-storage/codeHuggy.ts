bfce91ccc587100431e9c0f828a5e2ead9f4a788
luizPaim <luizhenriquesantospaim@gmail.com>
2023-03-08 10:06:06 -0300

    lib

--- src/modules-local-storage/codeHuggy.ts
+++
@@ -1,5 +0,0 @@
-export const SetLocalCodeHuggy = (codeHuggy: string) => {
-  localStorage.setItem('code', JSON.stringify(codeHuggy))
-}
-export const GetLocalCodeHuggy = () => localStorage.getItem('code')
-export const DeleteLocalCodeHuggy = () => localStorage.removeItem('code')
